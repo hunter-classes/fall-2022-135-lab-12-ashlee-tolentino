8300ca91dab3811c5ac23184e7ace12c614151ae
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-08 18:56:55 -0500

    added task b example

--- main.cpp
+++ main.cpp
@@ -19,9 +19,22 @@ int main()
   for(int i = 0; i < v1.size(); i++) {
     std::cout << v1[i] << std::endl;
   }
+  std::cout << "--------------------------------\n";
 
   // Task B
-
+  std::cout << "Task B\n\n";
+  std::vector<int> vect;
+  vect.push_back(1);
+  vect.push_back(2);
+  vect.push_back(-1);
+  vect.push_back(3);
+  vect.push_back(4);
+  vect.push_back(-1);
+  vect.push_back(6);
+  std::vector<int> v2 = goodVibes(vect);
+  for(int i = 0; i < v2.size(); i++) {
+    std::cout << v2[i] << std::endl;
+  }
   
   return 0;
 }
