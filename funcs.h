a3b50272b1b3a8560331af4182cd3451d870d787
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-08 19:48:42 -0500

    added task d prototype

--- funcs.h
+++ funcs.h
@@ -5,3 +5,4 @@
 std::vector<int> makeVector(int n);
 std::vector<int> goodVibes(const std::vector<int> v);
 void gogeta(std::vector<int> &goku, std::vector<int> &vegeta);
+std::vector<int> sumPairWise(std::vector<int> &v1, std::vector<int> &v2);
